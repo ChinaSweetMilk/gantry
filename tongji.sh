da376f67cf072be6b16cb43e35e0fb9fb86eb491
Your Name <you@example.com>
2020-02-26 16:17:12 +0800

    增加分开统计两种通行介质

--- tongji.sh
+++ tongji.sh
@@ -1,6 +1,7 @@
 #--------------------------------------------
 # author：李威远
-# date:2020/02/25 19:00
+# date:2020/02/26 15:22
+# version:2020/02/26 15:22
 #--------------------------------------------
 #!/bin/bash
 
@@ -9,14 +10,22 @@ MediaType=(ETC CPC);
 : > ${MediaType[0]}result
 : > ${MediaType[1]}result
 
-SpecialType=(101 102 103 104 105 106 107 108 109 116 117 118 119 120 121 122 123 130 131 132 135 136 137 138 145 146 147 148 149 150 155 156 157 158 159 160 161 162 163 164 165 166 167 168 169 170 171 172 173 174 175 176 177 178 179 180 181 185 186 187 188 189 190 191 192 193);
+SpecialType=(104 105 107 108 109 116 117 120 121 122 123 131 132 138 145 146 147 148 149 150 155 156 157 158 159 160 161 162 163 164 165 166 167 168 169 170 171 172 173 174 175 176 177 178 179 180 181 187 188 189 190 191 192 101 102 103 106 108 119 130 135 136 137 185 186 193);
 
-for ((j=0; j<=1; j ++))
+for ((j=0; j<2; j ++))
 do
 for ((i=0; i<=65; i ++))
 do
-  cat 21_* | grep -E "交易特情:${SpecialType[$i]} \[实时成功率\(小时\)\] $MediaType"|wc -l > ${MediaType}${SpecialType[$i]}
-
+  if [ ${SpecialType[$i]} == 104 ];then
+  echo -e "\033[31m====================${MediaType[j]} 交易失败分析====================\033[0m" >> ${MediaType[j]}result
+  echo -e "\033[31m\n=======异常错误=======\033[0m" >> ${MediaType[j]}result
+  echo -e "\033[31m\n特情码 数量\033[0m" >> ${MediaType[j]}result
+  elif [ ${SpecialType[$i]} = 101 ];then
+  echo -e "\033[32m\n=======正常错误=======\033[0m" >> ${MediaType[j]}result
+  fi
+  
+  cat 21_* | grep -E "交易特情:${SpecialType[$i]} \[实时成功率\(小时\)\] ${MediaType[j]}"|wc -l > ${MediaType[j]}${SpecialType[$i]}
+  
   sum=0
   while read line
   do
@@ -24,7 +33,20 @@ do
   done  < ${MediaType[j]}${SpecialType[$i]}
   echo "${SpecialType[$i]}    $sum" >> ${MediaType[j]}result
 done
-echo -e "\033[32m\n====================${MediaType[j]} 交易失败分析====================\033[0m"
-echo -e "\033[32m\n特情码 数量\033[0m"
+
 cat ${MediaType[j]}result
 done
+
+for ((j=0; j<2; j ++))
+do
+for ((i=0; i<=65; i ++))
+do
+  rm -rf ${MediaType[j]}${SpecialType[$i]}
+done
+done
+
+echo -e "\033[32m\n=====================================================\033[0m";
+echo "收到[b2]帧次数： `cat 21_* | grep -E "收到\[b2\]帧"|wc -l`";
+echo "发送[c6]帧次数： `cat 21_* | grep -E "发送\[c6\]帧"|wc -l`";
+echo "交易结果成功次数： `cat 21_* | grep -E "交易结果\:成功"|wc -l`";
+echo "交易结果失败次数： `cat 21_* | grep -E "交易结果\:失败"|wc -l`";
